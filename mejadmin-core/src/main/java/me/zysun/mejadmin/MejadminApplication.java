b4b9cd3535c0ff84b7b199051a39435e03eddd4d
Zhongyang Sun <z.y.sun@hotmail.com>
2023-02-11 21:11:23 +1100

    Project Initiation

---
+++ mejadmin-core/src/main/java/me/zysun/mejadmin/MejadminApplication.java
@@ -0,0 +1,13 @@
+package me.zysun.mejadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MejadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MejadminApplication.class, args);
+	}
+
+}
---
+++ mejadmin-core/src/test/java/me/zysun/mejadmin/MejadminApplicationTests.java
@@ -0,0 +1,13 @@
+package me.zysun.mejadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MejadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
